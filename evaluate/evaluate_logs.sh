ecb8d44d7aa3d3a0805a0ddc5c21b202ff4bf30d
Leonardo Piñeyro <leopiney@gmail.com>
2018-05-22 10:35:17 -0300

    updated evaluate logs script

--- evaluate/evaluate_logs.sh
+++ evaluate/evaluate_logs.sh
@@ -6,6 +6,10 @@ awk -f extract_logs.awk evaluate_random_direction_3.log > evaluate_random_direct
 awk -f extract_logs.awk evaluate_random_direction_4.log > evaluate_random_direction_4.csv
 awk -f extract_logs.awk evaluate_random_direction_5.log > evaluate_random_direction_5.csv
 awk -f extract_logs.awk evaluate_random_direction_6.log > evaluate_random_direction_6.csv
+awk -f extract_logs.awk evaluate_random_direction_6.log > evaluate_random_direction_7.csv
+awk -f extract_logs.awk evaluate_random_direction_6.log > evaluate_random_direction_8.csv
+awk -f extract_logs.awk evaluate_random_direction_6.log > evaluate_random_direction_9.csv
+awk -f extract_logs.awk evaluate_random_direction_6.log > evaluate_random_direction_10.csv
 
 awk -f extract_logs.awk evaluate_random_waypoint.log > evaluate_random_waypoint.csv
 awk -f extract_logs.awk evaluate_random_waypoint_2.log > evaluate_random_waypoint_2.csv
@@ -13,3 +17,16 @@ awk -f extract_logs.awk evaluate_random_waypoint_3.log > evaluate_random_waypoin
 awk -f extract_logs.awk evaluate_random_waypoint_4.log > evaluate_random_waypoint_4.csv
 awk -f extract_logs.awk evaluate_random_waypoint_5.log > evaluate_random_waypoint_5.csv
 awk -f extract_logs.awk evaluate_random_waypoint_6.log > evaluate_random_waypoint_6.csv
+awk -f extract_logs.awk evaluate_random_waypoint_6.log > evaluate_random_waypoint_7.csv
+awk -f extract_logs.awk evaluate_random_waypoint_6.log > evaluate_random_waypoint_8.csv
+awk -f extract_logs.awk evaluate_random_waypoint_6.log > evaluate_random_waypoint_9.csv
+awk -f extract_logs.awk evaluate_random_waypoint_6.log > evaluate_random_waypoint_10.csv
+
+awk -f extract_logs.awk evaluate_custom_3.log > evaluate_custom_3.csv
+awk -f extract_logs.awk evaluate_custom_4.log > evaluate_custom_4.csv
+awk -f extract_logs.awk evaluate_custom_5.log > evaluate_custom_5.csv
+awk -f extract_logs.awk evaluate_custom_6.log > evaluate_custom_6.csv
+awk -f extract_logs.awk evaluate_custom_6.log > evaluate_custom_7.csv
+awk -f extract_logs.awk evaluate_custom_6.log > evaluate_custom_8.csv
+awk -f extract_logs.awk evaluate_custom_6.log > evaluate_custom_9.csv
+awk -f extract_logs.awk evaluate_custom_6.log > evaluate_custom_10.csv
