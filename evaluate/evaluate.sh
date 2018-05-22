86c2f3fb7c166581886402fd732669d97bed68ef
Leonardo Piñeyro <leopiney@gmail.com>
2018-05-22 10:33:45 -0300

    Added new evaluation logs

---
+++ evaluate/evaluate.sh
@@ -0,0 +1,18 @@
+python -m tracer.evaluate run random_waypoint > evaluate/evaluate_random_waypoint_7.log &
+python -m tracer.evaluate run random_waypoint > evaluate/evaluate_random_waypoint_8.log &
+python -m tracer.evaluate run random_waypoint > evaluate/evaluate_random_waypoint_9.log &
+python -m tracer.evaluate run random_waypoint > evaluate/evaluate_random_waypoint_10.log &
+
+python -m tracer.evaluate run random_direction > evaluate/evaluate_random_direction_7.log &
+python -m tracer.evaluate run random_direction > evaluate/evaluate_random_direction_8.log &
+python -m tracer.evaluate run random_direction > evaluate/evaluate_random_direction_9.log &
+python -m tracer.evaluate run random_direction > evaluate/evaluate_random_direction_10.log &
+
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_3.log &
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_4.log &
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_5.log &
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_6.log &
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_7.log &
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_8.log &
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_9.log &
+python -m tracer.evaluate run custom > evaluate/evaluate_custom_10.log &
